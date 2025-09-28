ef3307d1460103309a0add1f74695671f62c9da8
Eric Wolf <1983821+typetetris@users.noreply.github.com>
2025-09-28 12:59:51 +0200

    Remove prelude over engineering

--- crates/lib/src/lib.rs
+++ crates/lib/src/lib.rs
@@ -1,7 +1,3 @@
-/// ### Prelude
-/// Prelude includes all the required modules for using PSArc-lib in a standard context
-pub mod prelude;
-
 /// ### Archive
 /// The archive module contains structs and methods related to handling the root archive file
 pub mod archive;
